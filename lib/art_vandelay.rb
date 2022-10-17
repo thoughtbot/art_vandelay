05c92d938322e90e0e88ea584c739bc993805ab2
Steve Polito <stevepolitodesign@users.noreply.github.com>
2022-10-17 19:50:53 -0400

    Send email with CSV attachment (#2)
    This commit introduces a feature allowing the caller to send the exported CSV in
    an email.
    
    In order to keep this feature small, there is no custom mailer or template.
    Instead, we simply invoke `ActionMailer::Base.mail` and define the properties
    inline.

--- lib/art_vandelay.rb
+++ lib/art_vandelay.rb
@@ -2,7 +2,7 @@ require "art_vandelay/version"
 require "art_vandelay/engine"
 
 module ArtVandelay
-  mattr_accessor :filtered_attributes
+  mattr_accessor :filtered_attributes, :from_address
   @@filtered_attributes = [:passw, :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn]
 
   def self.setup
@@ -10,6 +10,7 @@ module ArtVandelay
   end
 
   class Export
+    # TODO attributes: self.filtered_attributes
     def initialize(records, export_sensitive_data: false, attributes: [])
       @records = records
       @export_sensitive_data = export_sensitive_data
@@ -20,9 +21,23 @@ module ArtVandelay
       CSV.parse(generate_csv, headers: true)
     end
 
+    def email_csv(to:, from: ArtVandelay.from_address, subject: "#{model_name} export", body: "#{model_name} export")
+      mailer = ActionMailer::Base.mail(to: to, from: from, subject: subject, body: body)
+      mailer.attachments[file_name.to_s] = csv.to_csv
+
+      mailer.deliver
+    end
+
     private
 
     attr_reader :records, :export_sensitive_data, :attributes
+
+    def file_name
+      prefix = model_name.downcase
+      timestamp = Time.current.in_time_zone("UTC").strftime("%Y-%m-%d-%H-%M-%S-UTC")
+
+      "#{prefix}-export-#{timestamp}.csv"
+    end
 
     def filtered_values(attributes)
       if export_sensitive_data
@@ -52,7 +67,11 @@ module ArtVandelay
     end
 
     def model
-      records.model_name.name.constantize
+      model_name.constantize
+    end
+
+    def model_name
+      records.model_name.name
     end
 
     def row(attributes)
