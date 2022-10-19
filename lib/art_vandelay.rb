18aa5812665b37c18b1b6e62d523a77840e47ef0
Steve Polito <stevepolito@hey.com>
2022-10-19 05:51:59 -0400

    Export data in batches
    This commit ensures data is exported in batches to avoid large CSV files
    or long running queries.
    
    The default batch size is 10,000 to align with the default value for
    [in_batches], but this default can be overridden.
    
    [in_batches]: https://api.rubyonrails.org/classes/ActiveRecord/Batches.html#method-i-in_batches

--- lib/art_vandelay.rb
+++ lib/art_vandelay.rb
@@ -2,41 +2,70 @@ require "art_vandelay/version"
 require "art_vandelay/engine"
 
 module ArtVandelay
-  mattr_accessor :filtered_attributes, :from_address
+  mattr_accessor :filtered_attributes, :from_address, :in_batches_of
   @@filtered_attributes = [:passw, :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn]
+  @@in_batches_of = 10000
 
   def self.setup
     yield self
   end
 
   class Export
+    class Result
+      attr_reader :csv_exports
+
+      def initialize(csv_exports)
+        @csv_exports = csv_exports
+      end
+    end
+
     # TODO attributes: self.filtered_attributes
-    def initialize(records, export_sensitive_data: false, attributes: [])
+    def initialize(records, export_sensitive_data: false, attributes: [], in_batches_of: ArtVandelay.in_batches_of)
       @records = records
       @export_sensitive_data = export_sensitive_data
       @attributes = attributes
+      @in_batches_of = in_batches_of
     end
 
     def csv
-      CSV.parse(generate_csv, headers: true)
+      csv_exports = []
+
+      if records.is_a?(ActiveRecord::Relation)
+        records.in_batches(of: in_batches_of) do |relation|
+          csv_exports << CSV.parse(generate_csv(relation), headers: true)
+        end
+      elsif records.is_a?(ActiveRecord::Base)
+        csv_exports << CSV.parse(generate_csv(records), headers: true)
+      end
+
+      Result.new(csv_exports)
     end
 
     def email_csv(to:, from: ArtVandelay.from_address, subject: "#{model_name} export", body: "#{model_name} export")
       mailer = ActionMailer::Base.mail(to: to, from: from, subject: subject, body: body)
-      mailer.attachments[file_name.to_s] = csv.to_csv
+      csv_exports = csv.csv_exports
+
+      csv_exports.each.with_index(1) do |csv, index|
+        if csv_exports.one?
+          mailer.attachments[file_name] = csv
+        else
+          mailer.attachments[file_name(suffix: "-#{index}")] = csv
+        end
+      end
 
       mailer.deliver
     end
 
     private
 
-    attr_reader :records, :export_sensitive_data, :attributes
+    attr_reader :records, :export_sensitive_data, :attributes, :in_batches_of
 
-    def file_name
+    def file_name(**options)
       prefix = model_name.downcase
       timestamp = Time.current.in_time_zone("UTC").strftime("%Y-%m-%d-%H-%M-%S-UTC")
+      suffix = options[:suffix]
 
-      "#{prefix}-export-#{timestamp}.csv"
+      "#{prefix}-export-#{timestamp}#{suffix}.csv"
     end
 
     def filtered_values(attributes)
@@ -47,11 +76,15 @@ module ArtVandelay
       end
     end
 
-    def generate_csv
+    def generate_csv(relation)
       CSV.generate do |csv|
         csv << header
-        records.each do |record|
-          csv << row(record.attributes)
+        if relation.is_a?(ActiveRecord::Relation)
+          relation.each do |record|
+            csv << row(record.attributes)
+          end
+        elsif relation.is_a?(ActiveRecord::Base)
+          csv << row(records.attributes)
         end
       end
     end
