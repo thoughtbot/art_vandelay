73c9127dfe6ead6bdb728173c5c79721e9fe1d95
Steve Polito <stevepolitodesign@users.noreply.github.com>
2022-10-19 07:26:18 -0400

    Export data in batches (#3)
    This commit ensures data is exported in batches to avoid large CSV files
    or long running queries.
    
    The default batch size is 10,000 to align with the default value for
    [in_batches], but this default can be overridden.
    
    [in_batches]: https://api.rubyonrails.org/classes/ActiveRecord/Batches.html#method-i-in_batches

--- test/art_vandelay_test.rb
+++ test/art_vandelay_test.rb
@@ -12,30 +12,52 @@ class ArtVandelayTest < ActiveSupport::TestCase
     test "it has the correct default values" do
       filtered_attributes = ArtVandelay.filtered_attributes
       from_address = ArtVandelay.from_address
+      in_batches_of = ArtVandelay.in_batches_of
 
       assert_equal(
         [:passw, :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn],
         filtered_attributes
       )
       assert_nil from_address
+      assert_equal 10000, in_batches_of
     end
   end
 
   class Export < ArtVandelayTest
     include ActionMailer::TestHelper
 
-    test "it returns a CSV::Table instance" do
+    test "it returns a ArtVandelay::Export::Result instance" do
       User.create!(email: "user@xample.com", password: "password")
 
       result = ArtVandelay::Export.new(User.all).csv
 
-      assert_instance_of CSV::Table, result
+      assert_instance_of ArtVandelay::Export::Result, result
     end
 
     test "it creates a CSV containing the correct data" do
       user = User.create!(email: "user@xample.com", password: "password")
 
-      csv = ArtVandelay::Export.new(User.all).csv
+      result = ArtVandelay::Export.new(User.all).csv
+      csv = result.csv_exports.first
+
+      assert_equal(
+        [
+          ["id", "email", "password", "created_at", "updated_at"],
+          [user.id.to_s, user.email.to_s, "[FILTERED]", user.created_at.to_s, user.updated_at.to_s]
+        ],
+        csv.to_a
+      )
+      assert_equal(
+        ["id", "email", "password", "created_at", "updated_at"],
+        csv.headers
+      )
+    end
+
+    test "it creates a CSV when passed one record" do
+      user = User.create!(email: "user@xample.com", password: "password")
+
+      result = ArtVandelay::Export.new(User.first).csv
+      csv = result.csv_exports.first
 
       assert_equal(
         [
@@ -56,7 +78,7 @@ class ArtVandelayTest < ActiveSupport::TestCase
         config.filtered_attributes << :email
       end
 
-      csv = ArtVandelay::Export.new(User.all).csv
+      csv = ArtVandelay::Export.new(User.all).csv.csv_exports.first
 
       assert_equal(
         [
@@ -71,7 +93,7 @@ class ArtVandelayTest < ActiveSupport::TestCase
     test "it allows for unfiltered exports" do
       user = User.create!(email: "user@xample.com", password: "password")
 
-      csv = ArtVandelay::Export.new(User.all, export_sensitive_data: true).csv
+      csv = ArtVandelay::Export.new(User.all, export_sensitive_data: true).csv.csv_exports.first
 
       assert_equal(
         [
@@ -85,7 +107,7 @@ class ArtVandelayTest < ActiveSupport::TestCase
     test "it controlls what attributes are exported" do
       user = User.create!(email: "user@xample.com", password: "password")
 
-      csv = ArtVandelay::Export.new(User.all, attributes: [:id, "email"]).csv
+      csv = ArtVandelay::Export.new(User.all, attributes: [:id, "email"]).csv.csv_exports.first
 
       assert_equal(
         [
@@ -94,6 +116,35 @@ class ArtVandelayTest < ActiveSupport::TestCase
         ],
         csv.to_a
       )
+    end
+
+    test "it batches CSV exports" do
+      User.create!(email: "one@xample.com", password: "password")
+      User.create!(email: "two@xample.com", password: "password")
+
+      result = ArtVandelay::Export.new(User.all, in_batches_of: 1).csv
+      csv_1 = result.csv_exports.first
+      csv_2 = result.csv_exports.last
+
+      assert "one@example.com", csv_1.first["email"]
+      assert "two@example.com", csv_2.first["email"]
+    end
+
+    test "it can set the default batch size" do
+      User.create!(email: "one@xample.com", password: "password")
+      User.create!(email: "two@xample.com", password: "password")
+      ArtVandelay.setup do |config|
+        config.in_batches_of = 1
+      end
+
+      result = ArtVandelay::Export.new(User.all).csv
+      csv_1 = result.csv_exports.first
+      csv_2 = result.csv_exports.last
+
+      assert "one@example.com", csv_1.first["email"]
+      assert "two@example.com", csv_2.first["email"]
+
+      ArtVandelay.in_batches_of = 10000
     end
 
     test "it emails a CSV" do
@@ -124,7 +175,63 @@ class ArtVandelayTest < ActiveSupport::TestCase
       assert_equal "user-export-1989-12-31-00-00-00-UTC.csv", csv.filename
     end
 
+    test "it emails a CSV when one record is passed" do
+      travel_to Date.new(1989, 12, 31).beginning_of_day
+      user = User.create!(email: "user@xample.com", password: "password")
+
+      assert_emails 1 do
+        ArtVandelay::Export.new(User.first).email_csv(
+          to: ["recipient_1@examaple.com", "recipient_2@example.com"],
+          from: "sender@example.com"
+        )
+      end
+
+      email = ActionMailer::Base.deliveries.last
+      csv = email.attachments.first
+
+      assert_equal(
+        ["recipient_1@examaple.com", "recipient_2@example.com"],
+        email.to
+      )
+      assert_equal(
+        [
+          ["id", "email", "password", "created_at", "updated_at"],
+          [user.id.to_s, user.email.to_s, "[FILTERED]", user.created_at.to_s, user.updated_at.to_s]
+        ],
+        CSV.parse(csv.body.raw_source)
+      )
+      assert_equal "user-export-1989-12-31-00-00-00-UTC.csv", csv.filename
+    end
+
+    test "it emails multiple CSV attachments" do
+      travel_to Date.new(1989, 12, 31).beginning_of_day
+      User.create!(email: "one@example.com", password: "password")
+      User.create!(email: "two@example.com", password: "password")
+
+      assert_emails 1 do
+        ArtVandelay::Export.new(User.all, in_batches_of: 1).email_csv(
+          to: ["recipient_1@examaple.com"],
+          from: "sender@example.com"
+        )
+      end
+
+      email = ActionMailer::Base.deliveries.last
+      csv_1 = email.attachments.first
+      csv_2 = email.attachments.last
+
+      assert_match "one@example.com", csv_1.body.raw_source
+      assert_match "two@example.com", csv_2.body.raw_source
+      assert_equal "user-export-1989-12-31-00-00-00-UTC-1.csv", csv_1.filename
+      assert_equal "user-export-1989-12-31-00-00-00-UTC-2.csv", csv_2.filename
+    end
+
+    test "it raises an error if there is no data to export" do
+      skip
+    end
+
     test "it has a default subject" do
+      User.create!(email: "user@xample.com", password: "password")
+
       ArtVandelay::Export.new(User.all).email_csv(
         to: ["recipient_1@examaple.com", "recipient_2@example.com"],
         from: "sender@example.com"
@@ -135,6 +242,8 @@ class ArtVandelayTest < ActiveSupport::TestCase
     end
 
     test "it can set the subject" do
+      User.create!(email: "user@xample.com", password: "password")
+
       ArtVandelay::Export.new(User.all).email_csv(
         to: ["recipient_1@examaple.com", "recipient_2@example.com"],
         from: "sender@example.com",
@@ -146,6 +255,8 @@ class ArtVandelayTest < ActiveSupport::TestCase
     end
 
     test "it can set a from address" do
+      User.create!(email: "user@xample.com", password: "password")
+
       ArtVandelay::Export.new(User.all).email_csv(
         to: ["recipient_1@examaple.com", "recipient_2@example.com"],
         from: "FROM@EMAIL.COM"
@@ -156,6 +267,7 @@ class ArtVandelayTest < ActiveSupport::TestCase
     end
 
     test "it can set a default from address" do
+      User.create!(email: "user@xample.com", password: "password")
       ArtVandelay.setup do |config|
         config.from_address = "DEFAULT@EMAIL.COM"
       end
@@ -170,6 +282,7 @@ class ArtVandelayTest < ActiveSupport::TestCase
     end
 
     test "it has a default body" do
+      User.create!(email: "user@xample.com", password: "password")
       ArtVandelay::Export.new(User.all).email_csv(
         to: ["recipient_1@examaple.com", "recipient_2@example.com"],
         from: "sender@example.com"
@@ -180,6 +293,7 @@ class ArtVandelayTest < ActiveSupport::TestCase
     end
 
     test "it can set the body" do
+      User.create!(email: "user@xample.com", password: "password")
       ArtVandelay::Export.new(User.all).email_csv(
         to: ["recipient_1@examaple.com", "recipient_2@example.com"],
         from: "sender@example.com",
